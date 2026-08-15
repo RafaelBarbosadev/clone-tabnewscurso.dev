5be448635ab7ed9cd688132543a0992b220185d7
Rafael Barbosa <101080504+RafaelBarbosadev@users.noreply.github.com>
2026-08-15 00:44:58 +0000

    mudanças superficiais

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 function Home() {
-<<<<<<< HEAD
     return <h1> Construa seu objetivos abstraindo seus sonhos e depois defina seus passos para realiza-lós</h1>
-=======
-    return <h1> Construa seu objetivos abstraindo seus sonhos e depois defina seus passos para realiza-lósg=</h1>
->>>>>>> bea53d963a63be7191404a4094f0a87040f064af
+
 }
 
 export default Home;
